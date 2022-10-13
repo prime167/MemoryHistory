44fc39e8e042b922d41c497e317489162f7f9ba5
prime167 <3722845+prime167@users.noreply.github.com>
2022-10-13 10:57:46 +0800

    .

--- MemoryHistoryWpf/MainWindow.xaml.cs
+++ MemoryHistoryWpf/MainWindow.xaml.cs
@@ -1,183 +1,87 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
-using System.Threading;
 using System.Windows;
 using NLog;
 using ScottPlot;
-using ScottPlot.Plottable;
 
-namespace MemHistoryWpf
+namespace MemHistoryWpf;
+
+public partial class MainWindow : Window
 {
-    public partial class MainWindow : Window
+    private Logger _logger = LogManager.GetCurrentClassLogger();
+
+    private readonly List<ProcessPlotInfo> _processPlotInfos = new List<ProcessPlotInfo>(10);
+
+    public MainWindow()
     {
-        private Logger _logger = LogManager.GetCurrentClassLogger();
+        InitializeComponent();
+    }
 
-        private readonly List<ProcessPlotInfo> _processPlotInfos = new List<ProcessPlotInfo>(10);
+    private void MainWindow_OnLoaded(object sender, RoutedEventArgs e)
+    {
+        var me = Assembly.GetEntryAssembly()?.GetName().Name;
+        var ps1 = new ProcessPlotInfo("msedge", 600, WpfPlot1);
+        var ps2 = new ProcessPlotInfo("msedge", 60, WpfPlot2, true);
 
-        public MainWindow()
+        var ps3 = new ProcessPlotInfo(me, 600, WpfPlot3);
+        var ps4 = new ProcessPlotInfo(me, 60, WpfPlot4);
+
+        var ps5 = new ProcessPlotInfo("vivaldi", 600, WpfPlot5);
+        var ps6 = new ProcessPlotInfo("vivaldi", 60, WpfPlot6);
+
+        _processPlotInfos.Add(ps1);
+        _processPlotInfos.Add(ps2);
+        _processPlotInfos.Add(ps3);
+        _processPlotInfos.Add(ps4);
+        _processPlotInfos.Add(ps5);
+        _processPlotInfos.Add(ps6);
+
+        foreach (ProcessPlotInfo pi in _processPlotInfos)
         {
-            InitializeComponent();
-        }
+            pi.FormPlot.Configuration.DoubleClickBenchmark = false;
+            pi.FormPlot.Plot.Title($"{pi.Name}");
+            pi.FormPlot.Plot.YLabel("Memory (MB)");
+            pi.FormPlot.Plot.XLabel("Time (min)");
+            var sigMem = pi.FormPlot.Plot.AddSignal(pi.Mem);
+            sigMem.YAxisIndex = 0;
+            sigMem.XAxisIndex = 0;
 
-        private void MainWindow_OnLoaded(object sender, RoutedEventArgs e)
-        {
-            var me = Assembly.GetEntryAssembly().GetName().Name;
-            var ps1 = new ProcessPlotInfo("msedge", 120, WpfPlot1);
-            var ps2 = new ProcessPlotInfo("msedge", 12, WpfPlot2);
-
-            var ps3 = new ProcessPlotInfo(me, 120, WpfPlot3);
-            var ps4 = new ProcessPlotInfo(me, 12, WpfPlot4);
-
-            var ps5 = new ProcessPlotInfo("vivaldi", 120, WpfPlot5);
-            var ps6 = new ProcessPlotInfo("vivaldi", 12, WpfPlot6);
-
-            _processPlotInfos.Add(ps1);
-            _processPlotInfos.Add(ps2);
-            _processPlotInfos.Add(ps3);
-            _processPlotInfos.Add(ps4);
-            _processPlotInfos.Add(ps5);
-            _processPlotInfos.Add(ps6);
-
-            foreach (ProcessPlotInfo pi in _processPlotInfos)
+            if (pi.PlotProcessCount)
             {
-                pi.FormPlot.Configuration.DoubleClickBenchmark = false;
-                pi.FormPlot.Plot.Title($"{pi.Name}");
-                pi.FormPlot.Plot.YLabel("Memory (MB)");
-                pi.FormPlot.Plot.XLabel("Time (min)");
-
-                // Create another axis to the left and give it an index of 2
+                // 右侧Y轴，进程数量
                 pi.FormPlot.Plot.YAxis2.Ticks(true);
                 pi.FormPlot.Plot.YAxis2.Label("Porcess Count");
                 pi.FormPlot.Plot.YAxis2.Grid(false);
-                pi.FormPlot.Plot.YAxis2.MinimumTickSpacing(1);
-                var sigMem = pi.FormPlot.Plot.AddSignal(pi.Mem);
-                sigMem.YAxisIndex = 0;
-                sigMem.XAxisIndex = 0;
+                pi.FormPlot.Plot.YAxis2.MinimumTickSpacing(1); // 确保不出现小数
 
                 var sigPc = pi.FormPlot.Plot.AddSignal(pi.ProcessCount);
                 sigPc.YAxisIndex = 1;
                 sigPc.XAxisIndex = 0;
-
-                sigMem.FillBelow();
-                pi.SignalPlotMem = sigMem;
                 pi.SignalPlotPc = sigPc;
-                sigMem.MarkerSize = 0;
-                sigMem.MarkerShape = MarkerShape.filledCircle;
-
-                double[] xPositions = Enumerable.Range(0, 11).Select(e => e * 60.0 * pi.Time / 10).ToArray();
-                string[] xLabels = xPositions.Select(x => x / 60 + " m").Reverse().ToArray();
-                pi.FormPlot.Plot.XAxis.ManualTickPositions(xPositions, xLabels);
-
-                pi.StartPlot();
             }
-        }
-    }
 
-    public class ProcessPlotInfo
-    {
-        public string Name { get; set; }
+            sigMem.FillBelow();
+            pi.SignalPlotMem = sigMem;
+            sigMem.MarkerSize = 0;
+            sigMem.MarkerShape = MarkerShape.filledCircle;
 
-        public int Time { get; set; }
+            double[] xPositions;
+            string[] xLabels;
 
-        public readonly double[] Mem;
-        public readonly double[] ProcessCount;
-
-        public SignalPlot SignalPlotMem;
-        public SignalPlot SignalPlotPc;
-
-        public WpfPlot FormPlot;
-
-        private double memoryUsed;
-
-        private Timer updateMemoryTimer;
-        private Timer refreshTimer;
-
-        public ProcessPlotInfo(string processName, int time, WpfPlot formPlot)
-        {
-            Name = processName;
-            Time = time;
-            Mem = new double[60 * time];
-            ProcessCount = new double[60 * time];
-            FormPlot = formPlot;
-        }
-
-        public void GetMemory(object? state)
-        {
-            var ps = Process.GetProcessesByName(Name);
-            var total = 0.0;
-            foreach (var p in ps)
+            if (pi.Time <=60)
             {
-                if (!p.HasExited)
-                {
-                    try
-                    {
-                        if (Name == "msedge" && !p.MainModule.FileName.Contains("Dev"))
-                        {
-                            continue;
-                        }
-                    }
-                    catch (Exception)
-                    {
-                        continue;
-                    }
-
-                    p.Refresh();
-                    double s = 0.0;
-                    try
-                    {
-                        s = p.WorkingSet64 / 1024.0 / 1024.0;
-                        total += s;
-                    }
-                    catch (Exception)
-                    {
-
-                    }
-                }
+                xPositions = Enumerable.Range(0, 11).Select(e => e * 60.0 * pi.Time / 10).ToArray();
+                xLabels = xPositions.Select(x => x / 60 + " m").Reverse().ToArray(); // s=> min
             }
-
-            memoryUsed = Math.Round(total, 2);
-
-            // "scroll" the whole chart to the left
-            Array.Copy(Mem, 1, Mem, 0, Mem.Length - 1);
-            Array.Copy(ProcessCount, 1, ProcessCount, 0, ProcessCount.Length - 1);
-            Mem[^1] = memoryUsed;
-            ProcessCount[^1] = ps.Length;
-
-            try
+            else
             {
-                FormPlot.Dispatcher.Invoke(() =>
-                {
-                    FormPlot.Plot.AxisAuto();
-                    FormPlot.Refresh();
-                });
+                xPositions = Enumerable.Range(0, 11).Select(e => e * 60.0 * pi.Time / 10).ToArray();
+                xLabels = xPositions.Select(x => x / 3600 + " h").Reverse().ToArray();// s => hour
             }
-            catch (Exception e)
-            {
-            }
-        }
 
-        public void StartPlot()
-        {
-            updateMemoryTimer = new Timer(GetMemory, null, 0, 1000);
-            //refreshTimer = new Timer(RefreshPlot, null, 0, 1000);
-        }
-
-        private void RefreshPlot(object? state)
-        {
-            try
-            {
-                FormPlot.Dispatcher.Invoke(() =>
-                {
-                    FormPlot.Plot.AxisAuto();
-                    FormPlot.Refresh();
-                });
-            }
-            catch (Exception e)
-            {
-            }
+            pi.FormPlot.Plot.XAxis.ManualTickPositions(xPositions, xLabels);
+            pi.StartPlot();
         }
     }
 }
---
+++ MemoryHistoryWpf/ProcessPlotInfo.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+using ScottPlot;
+using ScottPlot.Plottable;
+
+namespace MemHistoryWpf;
+
+public class ProcessPlotInfo
+{
+    public string Name { get; set; }
+
+    public int Time { get; set; }
+
+    public readonly double[] Mem;
+    public readonly double[] ProcessCount;
+
+    public SignalPlot? SignalPlotMem;
+    public SignalPlot? SignalPlotPc;
+
+    public WpfPlot FormPlot;
+
+    private double memoryUsed;
+
+    private Timer? updateMemoryTimer;
+    private Timer? refreshTimer;
+
+    public bool PlotProcessCount { get; set; }
+
+    public ProcessPlotInfo(string processName, int time, WpfPlot formPlot, bool plotProcessCount = false)
+    {
+        Name = processName;
+        Time = time;
+        Mem = new double[60 * time];
+        ProcessCount = new double[60 * time];
+        FormPlot = formPlot;
+        PlotProcessCount = plotProcessCount;
+    }
+
+    public void GetMemory(object? state)
+    {
+        var ps = Process.GetProcessesByName(Name);
+        var total = 0.0;
+        foreach (var p in ps)
+        {
+            if (!p.HasExited)
+            {
+                try
+                {
+                    if (Name == "msedge" && !p.MainModule.FileName.Contains("Dev"))
+                    {
+                        continue;
+                    }
+                }
+                catch (Exception)
+                {
+                    continue;
+                }
+
+                p.Refresh();
+                double s = 0.0;
+                try
+                {
+                    s = p.WorkingSet64 / 1024.0 / 1024.0;
+                    total += s;
+                }
+                catch (Exception)
+                {
+
+                }
+            }
+        }
+
+        memoryUsed = Math.Round(total, 2);
+
+        // 左移曲线
+        Array.Copy(Mem, 1, Mem, 0, Mem.Length - 1);
+        Mem[^1] = memoryUsed;
+        if (PlotProcessCount)
+        {
+            Array.Copy(ProcessCount, 1, ProcessCount, 0, ProcessCount.Length - 1);
+            ProcessCount[^1] = ps.Length;
+        }
+
+        try
+        {
+            FormPlot.Dispatcher.Invoke(() =>
+            {
+                FormPlot.Plot.AxisAuto();
+                FormPlot.Refresh();
+            });
+        }
+        catch (Exception e)
+        {
+        }
+    }
+
+    public void StartPlot()
+    {
+        updateMemoryTimer = new Timer(GetMemory, null, 0, 1000);
+        //refreshTimer = new Timer(RefreshPlot, null, 0, 1000);
+    }
+
+    private void RefreshPlot(object? state)
+    {
+        try
+        {
+            FormPlot.Dispatcher.Invoke(() =>
+            {
+                FormPlot.Plot.AxisAuto();
+                FormPlot.Refresh();
+            });
+        }
+        catch (Exception e)
+        {
+        }
+    }
+}
