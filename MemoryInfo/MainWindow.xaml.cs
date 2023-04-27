4f3f6f415848bed5a517a3b6f4b8543429ad8e16
prime167 <3722845+prime167@users.noreply.github.com>
2023-04-27 14:44:28 +0800

    .

--- MemoryInfo/MainWindow.xaml.cs
+++ MemoryInfo/MainWindow.xaml.cs
@@ -12,19 +12,27 @@ public partial class MainWindow : Window
 {
     // ReSharper disable once NotAccessedField.Local
     private Timer _updateMemoryTimer;
+
+    /// <summary>
+    /// 显示的时间范围
+    /// </summary>
+    private const int MaxPeriod = 60 * 10;//60 * 30;
+
+    private static object _locker = new object();
     private const int k = 30;// sample Count;
     public List<double> Times = new();
-    public List<double> Percentages = new();
-    public List<double> Commits = new();
-    public List<double> CommitsAvg = new();
-    public List<double> CommitsSma= new();
-    private SimpleMovingAverage _sma;
 
-    private ScatterPlotList<double> _plot1;
-    private ScatterPlotList<double> _plot2;
-    private ScatterPlotList<double> _plot3;
-    private ScatterPlotList<double> _plot4;
-    private int _count;
+    public double[] Percentages = new double[MaxPeriod];
+    public double[] Commits = new double[MaxPeriod];
+    public double[] CommitsAvg = new double[MaxPeriod];
+    public double[] CommitsEma = new double[MaxPeriod];
+
+    private ExponentialMovingAverageIndicator _ema;
+
+    private SignalPlot _plotUsed;
+    private SignalPlot _plotCurrentCommit;
+    private SignalPlot _plotAvg;
+    private SignalPlot _plotEma;// ema
     private double _ppTitle = 0.0;
 
     /// <summary>
@@ -32,12 +40,9 @@ public partial class MainWindow : Window
     /// </summary>
     private const int MaxSeconds = 60 * 60 * 24 * 365;
 
-    /// <summary>
-    /// 显示的时间范围
-    /// </summary>
-    private const int MaxPeriod = 60 * 30;
-
     public MemoryViewModel Vm = new();
+    private Plot _plt1;
+    private Plot _plt2;
 
     public MainWindow()
     {
@@ -51,70 +56,77 @@ public partial class MainWindow : Window
 
     private void MainWindow_OnLoaded(object sender, RoutedEventArgs e)
     {
-        _sma = new SimpleMovingAverage(k);
+        _plt1 = WpUsed.Plot;
+        _plt2 = WpCommit.Plot;
+
+        _ema = new ExponentialMovingAverageIndicator(k);
         ResetCharts();
-        _plot1 = WpMemory.Plot.AddScatterList();
 
-        var plt = WpCommit.Plot;
-        _plot2 = plt.AddScatterList(label:"now",color:Color.Green);
-        _plot3 = plt.AddScatterList(label:"avg",color:Color.Blue);
-        _plot4 = plt.AddScatterList(label:"sma", color:Color.Orange);
-        plt.Legend(location:Alignment.UpperLeft);
+        _plotUsed = _plt1.AddSignal(Percentages, 1, color: Color.Green, label: "used");
+        _plotCurrentCommit = _plt2.AddSignal(Commits, 1, color: Color.Green, label: "now"); ;
+        _plotAvg = _plt2.AddSignal(CommitsAvg, 1, color: Color.Blue, label: "avg");
+        _plotEma = _plt2.AddSignal(CommitsEma, 1, color: Color.Red, label: "ema");
+        _plt2.Legend(location: Alignment.UpperLeft);
 
-        _plot1.MarkerSize = 1;
-        _plot2.MarkerSize = 1;
-        _plot3.MarkerSize = 1;
-        _plot4.MarkerSize = 1;
+        _plotUsed.MarkerSize = 1;
+        _plotCurrentCommit.MarkerSize = 1;
+        _plotAvg.MarkerSize = 1;
+        _plotEma.MarkerSize = 1;
         _updateMemoryTimer = new Timer(GetMemory, null, 0, 1000);
     }
 
     private void ResetCharts()
     {
-        WpMemory.Plot.Clear();
+        _plt1.Clear();
 
-        WpMemory.Configuration.DoubleClickBenchmark = false;
-        WpMemory.Plot.XAxis.SetBoundary(0, MaxSeconds);
-        WpMemory.Plot.XAxis.MinimumTickSpacing(1);
-        WpMemory.Plot.YAxis.SetBoundary(0, 100);
-        WpMemory.Plot.Grid();
-        WpMemory.MouseDoubleClick += WpMemory_MouseDoubleClick;
-        WpMemory.Plot.YLabel("使用中 (%)");
-        WpMemory.Plot.XLabel("时间 (s)");
-        WpMemory.Plot.Title("内存使用 %");
-        WpMemory.Refresh();
+        WpUsed.Configuration.DoubleClickBenchmark = false;
+        WpUsed.MouseDoubleClick += WpUsed_MouseDoubleClick;
 
-        WpCommit.Plot.Clear();
+        WpUsed.Plot.XAxis.SetBoundary(0, MaxSeconds);
+        _plt1.XAxis.MinimumTickSpacing(1);
+        _plt1.YAxis.SetBoundary(0, 100);
+        _plt1.Grid();
+        _plt1.YLabel("使用中 (%)");
+        _plt1.XLabel("时间 (s)");
+        _plt1.Title("内存使用 %");
+        WpUsed.Refresh();
+
+        _plt2.Clear();
         WpCommit.Configuration.DoubleClickBenchmark = false;
-        WpCommit.Plot.XAxis.SetBoundary(0, MaxSeconds);
-        WpCommit.Plot.XAxis.MinimumTickSpacing(1);
-        WpCommit.Plot.YAxis.SetBoundary(0, 100);
-        WpCommit.MouseDoubleClick += WpPageFile_MouseDoubleClick;
-        WpCommit.Plot.YLabel("已提交 (%)");
-        WpCommit.Plot.XLabel("时间 (s)");
-        WpCommit.Plot.Title("虚拟内存 %");
+        WpCommit.MouseDoubleClick += WpCommit_MouseDoubleClick;
+        _plt2.XAxis.SetBoundary(0, MaxSeconds);
+        _plt2.XAxis.MinimumTickSpacing(1);
+        _plt2.YAxis.SetBoundary(0, 100);
+        _plt2.YLabel("已提交 (%)");
+        _plt2.XLabel("时间 (s)");
+        _plt2.Title("虚拟内存 %");
         WpCommit.Refresh();
+    }
+
+    private void UpdateArray(double[] array, double last)
+    {
+        var len = array.Length;
+        Array.Copy(array, 1, array, 0, len - 1);
+        array[^1] = last;
     }
 
     public void GetMemory(object state)
     {
-        (double p1, double p2) = GetSystemMemoryUsagePercentage();
-        p1 = Math.Round(p1, 2);
-        p2 = Math.Round(p2, 2);
-
-        Times.Add(_count);
-        Percentages.Add(p1);
-        Commits.Add(p2);
-        CommitsAvg.Add(Math.Round(Commits.Average(),3));
-        var v = _sma.Update(p2);
-        CommitsSma.Add(v);
-
-        if (Times.Count > MaxPeriod)
+        double p1 = 0.0;
+        double p2 = 0.0;
+        lock (_locker)
         {
-            Times.RemoveAt(0);
-            Percentages.RemoveAt(0);
-            Commits.RemoveAt(0);
-            CommitsAvg.RemoveAt(0);
-            CommitsSma.RemoveAt(0);
+            (p1, p2) = GetSystemMemoryUsagePercentage();
+            p1 = Math.Round(p1, 2);
+            p2 = Math.Round(p2, 2);
+
+            UpdateArray(Percentages, p1);
+            UpdateArray(Commits, p2);
+            UpdateArray(CommitsAvg, Math.Round(Commits.Average(), 2));
+
+            _ema.AddDataPoint(p2);
+            var vv = _ema.Average;
+            UpdateArray(CommitsEma, vv);
         }
 
         Vm.CurrentPercentage = p1;
@@ -163,24 +175,10 @@ public partial class MainWindow : Window
                     Title = Math.Round(_ppTitle, 1) + "%";
                 }
 
-                _plot1.Clear();
-                _plot1.AddRange(Times.ToArray(), Percentages.ToArray());
-                WpMemory.Refresh();
-                WpMemory.Plot.AxisAuto();
-
-                _plot2.Clear();
-                _plot2.AddRange(Times.ToArray(), Commits.ToArray());
-
-                _plot3.Clear();
-                _plot3.AddRange(Times.ToArray(), CommitsAvg.ToArray());
-
-                _plot4.Clear();
-                _plot4.AddRange(Times.ToArray(), CommitsSma.ToArray());
-
-                WpCommit.Refresh();
-                WpCommit.Plot.AxisAuto();
-
-                _count++;
+                WpUsed.Render();
+                WpCommit.Render();
+                _plt1.AxisAuto();
+                _plt2.AxisAuto();
             });
         }
         catch (Exception e)
@@ -189,12 +187,12 @@ public partial class MainWindow : Window
         }
     }
 
-    private void WpMemory_MouseDoubleClick(object sender, System.Windows.Input.MouseButtonEventArgs e)
+    private void WpUsed_MouseDoubleClick(object sender, System.Windows.Input.MouseButtonEventArgs e)
     {
-        WpMemory.Plot.AxisAuto();
+        WpUsed.Plot.AxisAuto();
     }
 
-    private void WpPageFile_MouseDoubleClick(object sender, System.Windows.Input.MouseButtonEventArgs e)
+    private void WpCommit_MouseDoubleClick(object sender, System.Windows.Input.MouseButtonEventArgs e)
     {
         WpCommit.Plot.AxisAuto();
     }
